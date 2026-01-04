7f2ac493e901047e5f41f0e2a1cf76df43d56382
sisanator1 <ebigbeyisisan@gmail.com>
2026-01-04 16:02:11 -0500

    Adding a temp (on-page debug overlay via main.jsx) that i NEED to remove after i debug whats making the ios site crash

--- CineRadar/src/main.jsx
+++ CineRadar/src/main.jsx
@@ -4,6 +4,16 @@ import { AuthProvider } from "./AuthContext.jsx";
 import App from "./App.jsx";
 import "./index.css";
 
+/* 🔴 TEMPORARY MOBILE DEBUG OVERLAY */
+window.onerror = function (msg, src, line, col, err) {
+  alert(
+    "JS ERROR:\n" +
+    msg + "\n" +
+    "Line: " + line
+  );
+};
+/* 🔴 END DEBUG OVERLAY */
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
